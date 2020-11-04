42d747b051ca1fea676bb7c6ef5089052621bab8
Sam Marquart <marquart.sam@gmail.com>
2020-11-04 16:18:17 -0700

    updated the main example to use math types

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,48 @@
-import {addScriptHook, Timer, Unit, W3TS_HOOK} from 'w3lib';
+import {
+  addScriptHook,
+  degrees,
+  doAfter,
+  doPeriodically,
+  randomAngle,
+  Unit,
+  vec2,
+  W3TS_HOOK,
+} from 'w3lib';
 import {Players} from 'w3lib/src/globals/index';
 
 const BUILD_DATE = compiletime(() => new Date().toUTCString());
-// const TS_VERSION = compiletime(() => require('typescript').version);
-// const TSTL_VERSION = compiletime(() => require('typescript-to-lua').version);
 
 function tsMain() {
   print(`Build: ${BUILD_DATE}`);
-  // print(`Typescript: v${TS_VERSION}`);
-  // print(`Transpiler: v${TSTL_VERSION}`);
   print(' ');
   print('Welcome to TypeScript!');
 
-  const unit = new Unit(Players[0], FourCC('hfoo'), 0, 0, 270);
+  const pos = vec2(0, 0);
+  const unit = new Unit(Players[0], FourCC('hfoo'), pos, degrees(270));
   unit.name = 'TypeScript';
 
-  new Timer().start(1.0, true, () => {
+  const archer = new Unit(
+    Players[0],
+    FourCC('earc'),
+    vec2(0, 0),
+    randomAngle()
+  );
+
+  const cancel = doPeriodically(1, () => {
     unit.color = Players[math.random(0, bj_MAX_PLAYERS)].color;
+  });
+  let angle = degrees(0);
+  const cancelAnimation = doPeriodically(0.02, () => {
+    angle = angle.add(degrees(360 / 100));
+    const nextPos = unit.pos.polarOffset(angle, 300);
+    archer.pos = nextPos;
+    archer.facing = nextPos.angleTo(unit.pos);
+  });
+
+  doAfter(30, () => {
+    print('That was my set! Thanks for coming!');
+    cancel();
+    cancelAnimation();
   });
 }
 
