95f2f547d3b60780698ccb21fd3b5d6d7723cdb5
Sam Marquart <marquart.sam@gmail.com>
2020-11-12 19:08:23 -0700

    a more interesting main

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import {
   Unit,
   vec2,
   W3TS_HOOK,
+  unitId,
+  onAnyUnitSpellEffect,
+  abilId,
 } from 'w3lib';
 import {Players} from 'w3lib/src/globals/index';
 
@@ -18,21 +21,21 @@ function tsMain() {
   print('Welcome to TypeScript!');
 
   const pos = vec2(0, 0);
-  const unit = new Unit(Players[0], FourCC('hfoo'), pos, degrees(270));
+  const unit = new Unit(Players[0], unitId('hfoo'), pos, degrees(270));
   unit.name = 'TypeScript';
 
   const archer = new Unit(
     Players[0],
-    FourCC('earc'),
+    unitId('earc'),
     vec2(0, 0),
     randomAngle()
   );
 
-  const cancel = doPeriodically(1, () => {
+  const colorChange = doPeriodically(1, () => {
     unit.color = Players[math.random(0, bj_MAX_PLAYERS)].color;
   });
   let angle = degrees(0);
-  const cancelAnimation = doPeriodically(0.02, () => {
+  const animation = doPeriodically(0.02, () => {
     angle = angle.add(degrees(360 / 100));
     const nextPos = unit.pos.polarOffset(angle, 300);
     archer.pos = nextPos;
@@ -41,8 +44,15 @@ function tsMain() {
 
   doAfter(30, () => {
     print('That was my set! Thanks for coming!');
-    cancel();
-    cancelAnimation();
+    colorChange.cancel();
+    animation.cancel();
+  });
+
+  const banishId = abilId('AHbn');
+  onAnyUnitSpellEffect((caster, abilityId, target) => {
+    if (abilityId.equals(banishId) && target instanceof Unit) {
+      print(caster.name + ' banished ' + target.name + '!');
+    }
   });
 }
 
